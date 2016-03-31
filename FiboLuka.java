258714e058449aa714bcd76941fc0c99145ec415
Benjamin Rasic <brasic@student.tgm.ac.at>
2016-03-31 10:41:44 +0200

    first commit

---
+++ FiboLuka.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package rasic;
+
+/**
+ * Rekursiv Lukas zahlen
+ * Array für Fibonaci
+ * @author Rasic Benjamin
+ * @version 1.0
+ */
+public class FiboLuka {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Gibt Rekursiv Lucaszahlen zurück
+	 * @param n
+	 * @return
+	 */
+	public static long lucas(long n) {
+		try{
+	    if(n==0) 
+	    	return 2;
+	    if(n==1) 
+	    	return 1;
+	    return lucas(n+2) - lucas(n+1);
+		}catch(StackOverflowError e){
+			System.out.println("Overflow: ) ");
+			return n;
+		}catch(ArithmeticException e){
+			System.out.println("Unpassende Zahl");
+			return n;
+		}
+	}
+	
+	/**
+	 * Speichert Fibonacci Zahlen in array
+	 * @param n
+	 * @return 
+	 */
+	public long fibonacci(long n){
+		try{
+		if(n<=2)
+			return 1;
+		long[] fibo = new long[(int)n];
+        for (int i = 0; i < n; i++) {
+        	int f0 = 0;
+        	int f1 = 1;
+            int f2 = f0 + f1;
+            fibo[i] = f2;
+            f0 = f1;
+            f1 = f2;
+        }
+        return fibo[(int)n];
+		}catch(StackOverflowError e){
+			System.out.println("Overflow :) ");
+			return n;
+		}catch(ArithmeticException e){
+			System.out.println("Unpassende Zahl");
+			return n;
+		}
+	}
+
+}
